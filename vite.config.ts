b87bdb0cf9a5dd33c8ecaa84dfaef52d62e81ddf
wyw <740484522@qq.com>
2023-09-04 19:26:13 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import Icons from 'unplugin-icons/vite';
+import Inspect from 'vite-plugin-inspect';
 import IconsResolver from 'unplugin-icons/resolver';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
-import Inspect from 'vite-plugin-inspect';
 import legacyPlugin from '@vitejs/plugin-legacy';
 import VueDevTools from 'vite-plugin-vue-devtools';
 
@@ -19,40 +19,32 @@ export default defineConfig({
         },
     },
     plugins: [
-        VueDevTools(),
         Vue(),
-        AutoImport({
-            // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
-            imports: ['vue'],
-
-            // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
-            resolvers: [ElementPlusResolver()],
-
-            dts: path.resolve(pathSrc, 'types/auto-imports.d.ts'),
-        }),
-
-        Components({
-            resolvers: [
-                // 自动注册图标组件
-                IconsResolver({
-                    enabledCollections: ['ep'],
-                }),
-
-                // 自动导入 Element Plus 组件
-                ElementPlusResolver(),
-            ],
-
-            dts: path.resolve(pathSrc, 'types/components.d.ts'),
-        }),
-
         Icons({
             autoInstall: true,
         }),
+        AutoImport({
+            imports: ['vue', 'vue-router'],
+            resolvers: [ElementPlusResolver()],
+            dts: path.resolve(pathSrc, 'types/auto-imports.d.ts'),
+        }),
+        Components({
+            // 组件解析器
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    enabledCollections: ['ep'],
+                }),
+            ],
+
+            // 配置文件生成位置
+            dts: path.resolve(pathSrc, 'types/components.d.ts'),
+        }),
 
         Inspect(),
-
         legacyPlugin({
             targets: ['chrome 52'],
         }),
+        VueDevTools(),
     ],
 });
